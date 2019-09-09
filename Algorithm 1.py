dfd52f05c558b00d335829966f30daeae360e1be
zapatamezaj <zapatamezaj@berea.edu>
2019-09-09 09:02:12 -0400

    Bob is able to fill the screen. Sam needs to make an outline around Bob.

--- Algorithm 1.py
+++ Algorithm 1.py
@@ -1,4 +1,8 @@
 import turtle
+# import turtle files
+
+wn = turtle.Screen()
+# makes the screen for the turtle
 
 def draw_turtle(colr, sz):
     """
@@ -10,6 +14,34 @@ def draw_turtle(colr, sz):
     t.pensize(sz)
     return t
 
-bob = draw_turtle(back,25)
+bob = draw_turtle('red', 25)
+# draw the turtle given the color and size
 
-#next step is to make a window
+sam = draw_turtle('blue', 25)
+
+sam.penup()
+sam.setpos(-325, 275)
+sam.pendown()
+
+bob.penup()
+bob.setpos(-300, 250)
+bob.pendown()
+# move the origin of the turtle to the upper left hand side of the screen
+
+def move_turtle():
+    for i in range(13):
+        bob.forward(650)
+        bob.right(90)
+        bob.forward(25)
+        bob.right(90)
+        bob.forward(650)
+        bob.left(90)
+        bob.forward(25)
+        bob.left(90)
+# loop the movement of the turtle to make it fill the screen
+
+move_turtle()
+# start the loop
+
+wn.exitonclick()
+# end program by clicking on the screen
