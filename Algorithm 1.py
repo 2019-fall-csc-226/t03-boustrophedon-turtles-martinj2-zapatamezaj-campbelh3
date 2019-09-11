7ca8782c712318be9cf6694998f29443c9e0db03
zapatamezaj <zapatamezaj@berea.edu>
2019-09-11 08:37:09 -0400

    Completed the assignment. Everything works.

--- Algorithm 1.py
+++ Algorithm 1.py
@@ -1,8 +1,17 @@
+######################################################################
+# Author: Jose Zapata & Joey Martin
+# Username: zapatmezaj & martinj2
+#
+# Assignment: T03: Boustrophedon Turtles
+# Purpose: Introduces the use of functions with the turtle library
+######################################################################
+# Acknowledgements:
+# licensed under a Creative Commons
+# Attribution-Noncommercial-Share Alike 3.0 United States License.
+#################################################################################
 import turtle
 # import turtle files
 
-wn = turtle.Screen()
-# makes the screen for the turtle
 
 def draw_turtle(colr, sz):
     """
@@ -23,25 +32,40 @@ sam.penup()
 sam.setpos(-325, 275)
 sam.pendown()
 
+sam.forward(650)
+sam.right(90)
+sam.forward(500)
+sam.right(90)
+sam.forward(650)
+sam.right(90)
+sam.forward(500)
+
+
 bob.penup()
 bob.setpos(-300, 250)
 bob.pendown()
 # move the origin of the turtle to the upper left hand side of the screen
 
-def move_turtle():
-    for i in range(13):
-        bob.forward(650)
+def main():
+    wn = turtle.Screen()
+    # makes the screen for the turtle
+    for i in range(9):
+        bob.forward(600)
         bob.right(90)
         bob.forward(25)
         bob.right(90)
-        bob.forward(650)
+        bob.forward(600)
         bob.left(90)
         bob.forward(25)
         bob.left(90)
-# loop the movement of the turtle to make it fill the screen
+        # loop the movement of the turtle to make it fill the screen
+    bob.forward(600)
+    wn.exitonclick()
+    # end program by clicking on the screen
 
-move_turtle()
+main()
 # start the loop
 
-wn.exitonclick()
-# end program by clicking on the screen
+
+
+
